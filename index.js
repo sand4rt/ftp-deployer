3d4d5879e428559023103c581def9860f195b08f
sand4rt <sanderr@mbr.local>
2020-01-30 16:26:24 +0100

    Created ftp github-action

---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const FtpDeploy = require('ftp-deploy');
+
+core.info('Deploying...');
+
+new FtpDeploy()
+    .deploy({
+        host: core.getInput('user', { required: true }),
+        port: JSON.parse(core.getInput('port')) || 21,
+        user: core.getInput('username', { required: true }),
+        password: core.getInput('password', { required: true }),
+        remoteRoot: core.getInput('remote_folder', { required: true }),
+        localRoot: core.getInput('local_folder') || 'dist', // __dirname + '/local-folder',
+        deleteRemote: JSON.parse(core.getInput('cleanup')) || false, // If true, delete ALL existing files at destination before uploading
+        include: JSON.parse(core.getInput('include')) || [], // this would upload everything except dot files
+        exclude: JSON.parse(core.getInput('exclude')) || ['node_modules/**', 'node_modules/**/.*', '.git/**'], // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
+        forcePasv: JSON.parse(core.getInput('pasive')) || true // Passive mode is forced (EPSV command is not sent)
+    }) 
+    .then(response => core.log('Deploy finished:', response))
+    .catch(error => core.error(error));
