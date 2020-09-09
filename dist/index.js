854dcfa7468f688a0894865dd02f75fbeae2cb44
sand4rt <sanderr@mbr.local>
2020-09-09 11:03:40 +0200

    added included files to upload

--- dist/index.js
+++ dist/index.js
@@ -3114,7 +3114,7 @@ new FtpDeploy()
         remoteRoot: core.getInput('remote_folder') || './',
         localRoot: core.getInput('local_folder') || 'dist', // __dirname + '/local-folder',
         deleteRemote: JSON.parse(core.getInput('cleanup')) || false, // If true, delete ALL existing files at destination before uploading
-        include: JSON.parse(core.getInput('include')) || [], // this would upload everything except dot files
+        include: JSON.parse(core.getInput('include')) || ['*', '**/*'], // this would upload everything except dot files
         exclude: JSON.parse(core.getInput('exclude')) || ['node_modules/**', 'node_modules/**/.*', '.git/**'], // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
         forcePasv: JSON.parse(core.getInput('pasive')) || true // Passive mode is forced (EPSV command is not sent)
     }) 
